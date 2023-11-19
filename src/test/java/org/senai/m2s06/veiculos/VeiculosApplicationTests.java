aa3b09d15733e1281e679e6e0955a18bc05a59be
goptun <ramos.matheus@icloud.com>
2023-11-19 15:36:00 -0300

    [M02S07] Ex 01 - Mini-Projeto Veiculos - Criação projeto

---
+++ src/test/java/org/senai/m2s06/veiculos/VeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senai.m2s06.veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
